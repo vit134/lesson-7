73b5c7c1462b22fd923819c715a83248f045b28f
Vitaliy Andryushkov <vandryushkov@rbc.ru>
2018-04-04 16:19:49 +0300

    убрал все ошибки кампилятора

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 const hasOwnProperty = Object.prototype.hasOwnProperty;
-const toString = Object.prototype.toString;
+const toStringg = Object.prototype.toString;
 
 /**
  * Проверяет, что переданный объект является "плоским" (т.е. созданным с помощью "{}"
@@ -8,15 +8,14 @@ const toString = Object.prototype.toString;
  * @param {Object} obj
  * @returns {Boolean}
  */
-function isPlainObject(obj: object): boolean {
-    if (toString.call(obj) !== '[object Object]') {
+function isPlainObject(obj: object) {
+    if (toStringg.call(obj) !== '[object Object]') {
         return false;
     }
 
     const prototype = Object.getPrototypeOf(obj);
     return prototype === null ||
         prototype === Object.prototype;
-
 }
 
 /**
@@ -28,16 +27,13 @@ function isPlainObject(obj: object): boolean {
  *      `null` или `undefined` игнорируются.
  * @returns {Object}
  */
-const extend = function extend<T>(...args: T[]) {
-    let target: boolean | object = args[0];
-    let deep: boolean;
+const extend = function extend(...args: any[]):object {
+    let target = args[0];
+    let deep;
     let i;
-
-
 
     // Обрабатываем ситуацию глубокого копирования.
     if (typeof target === 'boolean') {
-
         deep = target;
         target = args[1];
         i = 2;
@@ -47,29 +43,25 @@ const extend = function extend<T>(...args: T[]) {
     }
 
     for (; i < arguments.length; i++) {
-        const obj: object = args[i];
+        const obj = args[i];
         if (!obj) {
             continue;
         }
 
         for (const key in obj) {
             if (hasOwnProperty.call(obj, key)) {
-                const val: any = obj[key];
-
+                const val = obj[key];
                 const isArray = val && Array.isArray(val);
 
                 // Копируем "плоские" объекты и массивы рекурсивно.
                 if (deep && val && (isPlainObject(val) || isArray)) {
                     const src = target[key];
-
                     let clone;
-
                     if (isArray) {
                         clone = src && Array.isArray(src) ? src : [];
                     } else {
                         clone = src && isPlainObject(src) ? src : {};
                     }
-
                     target[key] = extend(deep, clone, val);
                 } else {
                     target[key] = val;
@@ -78,18 +70,10 @@ const extend = function extend<T>(...args: T[]) {
         }
     }
 
-    console.log(target);
     return target;
 };
 
 
-console.log('--true--');
-let res = extend<[boolean | object, object]>([true, {}, 'asd', {asd: 'asd', bsd: 'bsd'}]);
+let res = extend(true, {}, {asd: 'asd', bsd: {asdasd: 'asdasd', bla: null}}, {aaa: 'aaa', bbb: 123});
+
 console.log(res);
-
-
-
-
-/*
-console.log('--obj--');
-console.log(extend(...arr2));*/
