06b5c8587d1a3dc3e7165b22b265f1d1de9b976f
vit134 <vit134@mail.ru>
2018-04-08 10:18:15 +0300

    доделал ts

--- index.ts
+++ index.ts
@@ -18,6 +18,10 @@ function isPlainObject(obj: object): boolean {
         prototype === Object.prototype;
 }
 
+interface Iobject extends Object{
+    [key: string]: any;
+}
+
 /**
  * Копирует перечислимые свойства одного или нескольких объектов в целевой объект.
  *
@@ -27,20 +31,31 @@ function isPlainObject(obj: object): boolean {
  *      `null` или `undefined` игнорируются.
  * @returns {Object}
  */
-const extend = function extend(deep: boolean, ...args: object[]) {
-    let target: object = args[1];
-    let i = deep ? 2 : 1;
+const extend = function extend(recursive: boolean | Iobject, ...args: object[]) {
+    let target: Iobject;
+    let deep: boolean;
+    let i;
 
-    for (; i < args.length; i++) {
-        const obj: object = args[i];
+    // Обрабатываем ситуацию глубокого копирования.
+    if (typeof recursive === 'boolean') {
+        deep = recursive;
+        target = args[0];
+        i = 1;
+    } else {
+        deep = false;
+        target = recursive;
+        i = 0;
+    }
+
+    for (; i < arguments.length; i++) {
+        const obj: Iobject = args[i];
         if (!obj) {
             continue;
         }
 
         for (const key in obj) {
             if (hasOwnProperty.call(obj, key)) {
-                const val: object = obj[key];
-                console.log(val);
+                const val: any = obj[key];
                 const isArray = val && Array.isArray(val);
 
                 // Копируем "плоские" объекты и массивы рекурсивно.
@@ -63,8 +78,8 @@ const extend = function extend(deep: boolean, ...args: object[]) {
     return target;
 };
 
-let res = extend(true, ...[{}, {aaa: 'aaa', bbb: 'bbb', asd: {dsa: 'dsa', qwe: 'ewq'}}, {ccc: null, undefined: 111, dfg: ['123', 321]}, {eee: 'eee', ddd: 'ddd'}]);
+let res = extend(true, {}, {aaa: 'aaa', bbb: 'bbb', asd: {dsa: 'dsa', qwe: 'ewq'}}, {ccc: null, undefined: 111, dfg: ['123', 321]}, {eee: 'eee', ddd: 'ddd'});
 
-//let res = extend(...[true, {}, {asd: 'asd', bsd: {asdasd: 'asdasd', bla: null, null: 'bla'}}, {aaa: 'aaa', bbb: 123}]);
+
 
 console.log(res);
